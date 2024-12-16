1b7a9f99feede0182720683aaaa55b5ccd8e2f9e
frlegionaire <valentinvanhelle@hotmail.fr>
2024-12-16 15:44:37 +0100

    ajout toggle

--- navBar js/js.js
+++ navBar js/js.js
@@ -1,21 +1,27 @@
- const burger = document.querySelector('.cacher');
+//  const burger = document.querySelector('.cacher');
  const nav = document.querySelector('.navbar_header');
- const menuburger = document.querySelector('.imgburgers')
+ const menuburger = document.querySelector('.navMenuBurg')
+ const divburger = document.querySelector('.imgburgers')
 
- burger.addEventListener('click', () => {
-    menuburger.classList.add('menuderoulant');
-    nav.classList.add('navbarHeader');
-    nav.classList.remove('navbar_header');
-    menuburger.classList.add('bidule');
+//  burger.addEventListener('click', () => {
+//     menuburger.classList.add('menuderoulant');
+//     nav.classList.add('navbarHeader');
+//     nav.classList.remove('navbar_header');
+//     menuburger.classList.add('bidule');
+//  });
+
+ menuburger.addEventListener('click', () => {
+    divburger.classList.toggle('menuderoulant');
+    nav.classList.toggle('navbarHeader');
+    menuburger.classList.toggle('bidule');
  });
 
-const croix = document.querySelector('.croix');
 
-croix.addEventListener('click', () => {
-    menuburger.classList.remove('menuderoulant');
-    nav.classList.remove('navbarHeader');
-    nav.classList.add('navbar_header');
-    menuburger.classList.remove('bidule');
-})
+// const croix = document.querySelector('.croix');
 
-console.log(burger.className);
+// croix.addEventListener('click', () => {
+//     menuburger.classList.remove('menuderoulant');
+//     nav.classList.remove('navbarHeader');
+//     nav.classList.add('navbar_header');
+//     menuburger.classList.remove('bidule');
+// })
