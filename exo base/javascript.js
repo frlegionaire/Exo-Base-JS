f1a4178e7c4eddf3f158e308dc35ad3ca97fbf89
frlegionaire <valentinvanhelle@hotmail.fr>
2024-12-17 12:00:21 +0100

    ajout de fonction

--- exo base/javascript.js
+++ exo base/javascript.js
@@ -47,3 +47,98 @@ function multiplication(multi){
 }
 
 console.log(multiplication("multi"));
+
+
+
+function majorite(age) {
+    if(age < 18 ) {
+        console.log("La personne est mineur");
+    }
+    else {
+        console.log("la personne est majeur");
+    } 
+}
+
+majorite(12);
+
+for (let i = 1; i <= 20; i++ ) {
+    if ( i % 2 === 0){
+        console.log(i);  
+    }
+
+}
+
+//  function devinerlechiffre() {
+//      let nomb = Math.floor(Math.random() * 100 + 1);
+//      let guess;
+
+// do {
+//          guess = prompt('Essayer de deviner le nombre entre 1 et 100');
+//          console.log(nomb, guess);
+//          if ( nomb > guess ) {
+//              alert('trop petit');
+//          } else if (guess > nomb) {
+//              alert('trop grand');
+//         }
+//     }while (nomb != guess);
+//     {
+//         alert('bravo');
+//     }
+//  }
+//  devinerlechiffre();
+
+
+function jourdanslemois(mois, annee) {
+    let mois;
+    mois = prompt('entrer le mois');
+    let annee;
+    annee = prompt("donne moi l'annee");
+
+    switch (mois)  {
+        case '1' :
+            return ("Il y a 31 jours au mois de janvier");
+            break;
+        case '2' :
+            if ( annee % 4 === 0 ){
+                return ("Pour le mois de février, celui-ci compte 29 jours.");
+                break;
+            } else {
+                return ("Pour le mois de février, celui-ci compte 28 jours.");
+                break;
+            }
+        case '3':
+            alert('Il y a 31 jours au mois de mars');
+            break;
+        case '4' :
+            alert("Il y a 30 jours au mois de d'avril");
+            break;
+        case '5' :
+            alert("Il y a 31 jours au mois de mai");
+            break;
+        case '6' :
+            alert("Il y a 30 jours au mois de juin");
+            break;
+        case '7' :
+            alert("Il y a 31 jours au mois de juillet");
+            break;
+        case '8' :
+            alert("Il y a 31 jours au mois d'aout");
+            break;
+        case '9' :
+            alert("Il y a 30 jours au mois de septembre");
+            break;
+        case '10' :
+            alert("Il y a 31 jours au mois de octobre");
+            break;
+        case '11' : 
+            alert("Il y a 30 jours au mois de novembre");
+            break;
+        case '12' : 
+            alert("Il y a 31 jours au mois de décembre");
+            break;
+        default : 
+            alert('Desole, votre réponse ne correspond pas à un mois');
+    }
+}
+jourdanslemois();
+
