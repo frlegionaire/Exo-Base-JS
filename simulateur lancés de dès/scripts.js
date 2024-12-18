c16c6becb3b10093785963c1ede265fb4a5ba21e
frlegionaire <valentinvanhelle@hotmail.fr>
2024-12-18 11:19:19 +0100

    ajout anim

--- simulateur lancés de dès/scripts.js
+++ simulateur lancés de dès/scripts.js
@@ -1,5 +1,13 @@
-function lancer() {
+const button = document.querySelector('button');
+const animation = document.querySelector('animation');
+button.addEventListener("click", () => {
+    setTimeout(() => {
+        dice.classList.add("animation");
+      }, 10);
+      dice.classList.remove("animation");
+
     let nomb = Math.floor(Math.random() * 6 + 1);
     const diceImage = document.getElementById('dice');
     diceImage.src = `des/dieWhite${nomb}.png`;
-}
+});
+
